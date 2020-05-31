01da0120ae2376b48a7b8e52e4f89f9c67436bfd
Georg Jung <git@gjung.com>
2020-05-31 15:43:04 +0300

    improve handaction

--- src/android/Haifa3d/app/src/main/java/com/gjung/haifa3d/model/HandAction.kt
+++ src/android/Haifa3d/app/src/main/java/com/gjung/haifa3d/model/HandAction.kt
@@ -3,23 +3,13 @@ package com.gjung.haifa3d.model
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-@Parcelize
-class HandAction() : Parcelable, ByteRepresentable {
-    private lateinit var repr: List<Byte>
-
-    constructor(
-        movements: Iterable<HandMovement>
-    ) : this() {
-        repr = movements.flatMap { it.toBytes() }
-    }
-
+class HandAction(val Movements: Collection<HandMovement>) : ByteRepresentable {
     constructor(
         vararg movements: HandMovement
-    ) : this() {
-        repr = movements.flatMap { it.toBytes() }
+    ) : this(movements.toList())
+
+    override fun toBytes(): Iterable<Byte> {
+        val repr = Movements.flatMap { it.toBytes() }
+        return LengthIndicator((repr.size + 1).toByte()).toBytes() + repr
     }
-
-    override fun toBytes(): Iterable<Byte> =
-        LengthIndicator((repr.size + 1).toByte()).toBytes() + repr
-
 }
