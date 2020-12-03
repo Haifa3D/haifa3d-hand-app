bfb3e3ea87ec791f209e28ce7375aa04436ccd00
Niv Levi <levi.niv@campus.technion.ac.il>
2020-12-03 11:51:03 +0200

    removed drawer added bottom  #9

--- src/android/Haifa3d/app/src/main/java/com/gjung/haifa3d/ConnectedActivity.kt
+++ src/android/Haifa3d/app/src/main/java/com/gjung/haifa3d/ConnectedActivity.kt
@@ -10,11 +10,22 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.gjung.haifa3d.databinding.ActivityConnectedBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import android.widget.PopupMenu
+import kotlinx.android.synthetic.main.activity_connected.*
+import kotlinx.android.synthetic.main.app_bar_connected.*
+
 
 class ConnectedActivity : BleActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityConnectedBinding
+
     private var isInFront = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,20 +33,57 @@ class ConnectedActivity : BleActivity() {
         binding = ActivityConnectedBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.appbar.toolbar)
+        val navView: NavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_livecontrol, R.id.nav_presets, R.id.nav_about, R.id.nav_configuration
             ), binding.drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.navView.setupWithNavController(navController)
 
         binding.disconnectButton.setOnClickListener { disconnect() }
+
+        val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+
+        toolbar_save.setOnClickListener {
+            showPopup(toolbar_save)
+        }
     }
+
+    private fun showPopup(view: View) {
+        val popup = PopupMenu(this, view)
+        popup.inflate(R.menu.menu_main)
+
+        popup.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item: MenuItem? ->
+
+            when (item!!.itemId) {
+
+                R.id.nav_about -> {
+                    Toast.makeText(this@ConnectedActivity, item.title, Toast.LENGTH_SHORT).show()
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.nav_about)
+                }
+                R.id.disconnect_button -> {
+                    Toast.makeText(this@ConnectedActivity, item.title, Toast.LENGTH_SHORT).show()
+                    disconnect()
+                }
+            }
+
+            true
+        })
+
+        popup.show()
+    }
+
+
+
+
+
 
     private fun disconnect() {
         bleService?.manager?.disconnect()
@@ -69,10 +117,27 @@ class ConnectedActivity : BleActivity() {
         finish()
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
+
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.nav_home)
+                }
+                R.id.nav_livecontrol -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.nav_livecontrol)
+                }
+                R.id.nav_presets -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.nav_presets)
+                }
+                R.id.nav_configuration -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.nav_configuration)
+                }
+
+            }
+            false
+        }
+
 
     override fun onResume() {
         super.onResume()
